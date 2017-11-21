ed0d6254fb31e28d995f365acb77d5fb274f80be
Valera Gorobets <valera.gorobets@gmai.com>
2017-11-21 09:15:45 +0300

    Init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import timeseries from 'timeseries-analysis';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+	public data = [12,16,14,13,11,10,9,11,23];
+ 
+	public t     = new timeseries.main(timeseries.adapter.fromArray(this.data));
+	chart_url = this.t.ma({period: 14}).chart();
+
+  
 }

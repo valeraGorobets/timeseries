a66b3945d27818189242c20835e002054e761648
Valera Gorobets <valera.gorobets@gmai.com>
2017-11-21 10:09:16 +0300

    heroku deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,16 @@ import { Component } from '@angular/core';
 import timeseries from 'timeseries-analysis';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
-	public data = [12,16,14,13,11,10,9,11,23];
- 
-	public t     = new timeseries.main(timeseries.adapter.fromArray(this.data));
-	chart_url = this.t.ma({period: 14}).chart();
+	title = 'app';
+	public data = [10, 12, 16, 14, 13, 11, 10, 9, 11, 23];
 
-  
+	public t = new timeseries.main(timeseries.adapter.fromArray(this.data));
+	chart_url = this.t.ma({ period: 14 }).chart();
+
+
 }
